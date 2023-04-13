e86f66cc9dfb3a9898745f127aa3faf42b4ced90
tohbey <fafoworatobi25@gmail.com>
2023-04-13 20:25:44 +0100

    - addition of interview controller

---
+++ src/main/java/com/interview/interviewservice/controller/InterviewController.java
@@ -0,0 +1,84 @@
+package com.interview.interviewservice.controller;
+
+
+import com.interview.interviewservice.Util.GlobalMessage;
+import com.interview.interviewservice.Util.IDataResponse;
+import com.interview.interviewservice.mapper.DTOS.InterviewDTO;
+import com.interview.interviewservice.model.Message;
+import com.interview.interviewservice.service.InterviewService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping(value = InterviewController.BASE_URL)
+public class InterviewController {
+
+    public static final String BASE_URL="/api/v1/interview";
+
+    private final InterviewService interviewService;
+
+    public InterviewController(InterviewService interviewService) {
+        this.interviewService = interviewService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/save")
+    public IDataResponse createInterview(@RequestBody InterviewDTO interviewDTO){
+        IDataResponse dataResponse = new IDataResponse();
+        try{
+            interviewService.create(interviewDTO);
+            dataResponse.setValid(true);
+            dataResponse.addMessage(new GlobalMessage("Interview Successfully Created","Saved", Message.Severity.INFO));
+        }catch (Exception e){
+            e.printStackTrace();
+            dataResponse.setValid(false);
+            dataResponse.addMessage(new GlobalMessage(e.getMessage(), null, Message.Severity.ERROR));
+        }
+        return dataResponse;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/find/{interviewId}")
+    public IDataResponse findInterview(@PathVariable("interviewId") Long interviewId){
+        IDataResponse dataResponse = new IDataResponse();
+        try{
+            dataResponse.setData(Collections.singletonList(interviewService.findInterview(interviewId)));
+            dataResponse.setValid(true);
+            dataResponse.addMessage(new GlobalMessage("Interview Successfully Retrieved","Retrieved", Message.Severity.INFO));
+        }catch (Exception e){
+            e.printStackTrace();
+            dataResponse.setValid(false);
+            dataResponse.addMessage(new GlobalMessage(e.getMessage(), null, Message.Severity.ERROR));
+        }
+        return dataResponse;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{interviewId}")
+    public IDataResponse deleteInterview(@PathVariable("interviewId") Long interviewId){
+        IDataResponse dataResponse = new IDataResponse();
+        try{
+            interviewService.delete(interviewId);
+            dataResponse.setValid(true);
+            dataResponse.addMessage(new GlobalMessage("Interview Successfully Deleted","Deleted", Message.Severity.INFO));
+        }catch (Exception e){
+            e.printStackTrace();
+            dataResponse.setValid(false);
+            dataResponse.addMessage(new GlobalMessage(e.getMessage(), null, Message.Severity.ERROR));
+        }
+        return dataResponse;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/update")
+    public IDataResponse updateInterview(@RequestBody InterviewDTO interviewDTO){
+        IDataResponse dataResponse = new IDataResponse();
+        try{
+            interviewService.update(interviewDTO);
+            dataResponse.setValid(true);
+            dataResponse.addMessage(new GlobalMessage("Interview Successfully Updated","Updated", Message.Severity.INFO));
+        }catch (Exception e){
+            e.printStackTrace();
+            dataResponse.setValid(false);
+            dataResponse.addMessage(new GlobalMessage(e.getMessage(), null, Message.Severity.ERROR));
+        }
+        return dataResponse;
+    }
+}
